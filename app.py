2654b7737785233ee728a68427a2b5f25a5b5325
Maayan <93875493+Maayan6060@users.noreply.github.com>
2024-11-27 16:13:09 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,5 +5,5 @@ from PIL import Image
 st.set_page_config(page_title="אלתרמן חלל ותעופה", page_icon=":school:", layout="wide")
 
 #Top page image
-image = Image.open(r"C:\Users\Maayan\Desktop\School Website\school.jpg")
-st.image(r"C:\Users\Maayan\Desktop\School Website\school.jpg", caption='בית ספר לחלל ותעופה ע"ש אלתרמן', use_container_width=True)
+image = Image.open("school.jpg")
+st.image("school.jpg", caption='בית ספר לחלל ותעופה ע"ש אלתרמן', use_container_width=True)
