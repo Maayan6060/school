b753205db502e5866914c6698948e8a77c9c811a
Maayan <93875493+Maayan6060@users.noreply.github.com>
2024-11-28 21:02:19 +0200

    Update app.py

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ st.markdown(
         position: relative;
         width: 100%;
         height: 30px; 
-        background-color: rgba(255, 255, 255, 0.1);
+        background-color: rgba(255, 255, 255, 0.9);
         margin: 20px 0; 
     }
     </style>
