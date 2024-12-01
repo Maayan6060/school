adf44184bb38a9eda2f545eee578d4c38e216ecb
Maayan <93875493+Maayan6060@users.noreply.github.com>
2024-12-01 14:59:59 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,22 @@
 # import streamlit as st
 # from PIL import Image
 
-# #tab title
-# #tab title
+# # Tab title
 # st.set_page_config(page_title="אלתרמן חלל ותעופה", page_icon=":school:", layout="wide")
 
-# #search engine optimization
-# st.markdown("""
+# # Search engine optimization
+# st.markdown(
+#     """
 #       <meta name="google-site-verification" content="NMpko4mHDXRG7bRxWRz9ZaLISwNTjC3ckDQv3zPufI4" />
 #       <meta name="this is a school website. it contains information about the school, tests, assays, and exam dated for the students, custom AI to help students with multiple school subjexts and more" content="Alterman - A web app built using Streamlit">
 #       <meta name="Alterman" content="Alterman, Streamlit app, Web app, Example">
 #       <meta name="author" content="Your Name">
 #       <title>Alterman</title>
-#  """, unsafe_allow_html=True)
+#      """,
+#     unsafe_allow_html=True
+# )
 
-# #Top page image
+# # Top page image
 # background_image_url = "https://www.netanya.muni.il/DocLib/%D7%97%D7%99%D7%A0%D7%95%D7%9A/%D7%9E%D7%95%D7%A1%D7%93%D7%95%D7%AA/%D7%A2%D7%9C%20%D7%99%D7%A1%D7%95%D7%93%D7%99/%D7%AA%D7%96/%D7%91%D7%99%D7%AA%20%D7%94%D7%A1%D7%A4%D7%A8%20%D7%9C%D7%AA%D7%A2%D7%95%D7%A4%D7%94%20%D7%95%D7%9C%D7%97%D7%9C%D7%9C%20%D7%A2%D7%9C%20%D7%A9%D7%9D%20%D7%90%D7%9C%D7%AA%D7%A8%D7%9E%D7%9F/28.jpg?RenditionID=7"
 # st.markdown(
 #     f"""
@@ -24,16 +26,14 @@
 #         background-size: 100% 80%; 
 #         background-repeat: no-repeat; 
 #         background-position: center; 
-#         background-attachment: scroll; 
+#         background-attachment: scroll !important; 
 #     }}
 #     </style>
 #     """,
 #     unsafe_allow_html=True
 # )
 
-
-
-# #horizontal image line+text
+# # Horizontal image line + text
 # st.markdown(
 #     """
 #     <style>
@@ -65,25 +65,23 @@
 #     unsafe_allow_html=True
 # )
 
-# #Text - paragraph 1
+# # Text - paragraph 1
 # st.markdown(
 #     """
 #     <style>
 #     .custom-text {
-#         position: relative;
-#         bottom: -10000px; 
-#         left: 50px; 
+#         position: absolute;
+#         margin-top: 10000px;
 #         font-size: 20px; 
 #         color: black;
 #     }
 #     </style>
 #     <div class="custom-text">
-#         rrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr.
+#         rrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr.
 #     </div>
 #     """,
 #     unsafe_allow_html=True
 # )
-
 import streamlit as st
 from PIL import Image
 
@@ -109,10 +107,9 @@ st.markdown(
     <style>
     .stApp {{
         background-image: url("{background_image_url}");
-        background-size: 100% 80%; 
-        background-repeat: no-repeat; 
-        background-position: center; 
-        background-attachment: scroll !important; 
+        background-size: cover;
+        background-repeat: no-repeat;
+        background-position: center;
     }}
     </style>
     """,
@@ -125,8 +122,7 @@ st.markdown(
     <style>
     .horizontal-line {
         position: relative;
-        right: 1000px;
-        width: 200%;
+        width: 100%;
         height: 80px; 
         background-color: rgba(255, 255, 255, 0.75);
         margin: 20px 0; 
@@ -143,10 +139,8 @@ st.markdown(
     }
     </style>
     <div class="horizontal-line">
-    <div style="position: relative; z-index: 1000;">
-       <div class="line-text">חטיבת ביניים חלל ותעופה ע"ש אלתרמן</div>
+    <div class="line-text">חטיבת ביניים חלל ותעופה ע"ש אלתרמן</div>
     </div>
-
     """,
     unsafe_allow_html=True
 )
@@ -156,18 +150,18 @@ st.markdown(
     """
     <style>
     .custom-text {
-        position: absolute;
-        margin-top: 10000px;
+        margin: 20px;
         font-size: 20px; 
         color: black;
     }
     </style>
     <div class="custom-text">
-        rrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse auctor velit nec diam blandit, at eleifend risus dignissim.
     </div>
     """,
     unsafe_allow_html=True
 )
+
 
 
 
