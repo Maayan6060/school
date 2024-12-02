aa5d944deabecce7aae35f769000273424d7a73a
Maayan <93875493+Maayan6060@users.noreply.github.com>
2024-12-02 16:19:54 +0200

    Update app.py

--- app.py
+++ app.py
@@ -8,5 +8,10 @@ app.layout = html.Div([
     html.Img(src="/assets/natanya.jpg", alt="Natanya Logo", style={"width": "8%", "height": "12vh", "position": "fixed", 'top': "7%", "left": "45%", "transform": "translate(-50%,-50%)", "z-index": "1000", }),
     html.Img(src="/assets/alterman.jpg", alt="Alterman Logo", style={"width": "20%", "height": "25vh", "position": "fixed", 'top': "7%", "left": "55%", "transform": "translate(-50%,-50%)", "z-index": "1000", })
 ])
-if __name__ == '__main__':
-    app.run_server(debug=True)
+import os
+
+# Get the port number from the environment variable, default to 8050 if not set
+port = int(os.getenv('PORT', 8050))
+
+# Run the Dash app on 0.0.0.0 and the port provided by Render
+app.run_server(host='0.0.0.0', port=port, debug=True)
